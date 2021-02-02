c6013a59fda6c5fe56013e6904340702334c2edd
Daniel <Newsom>
2021-02-02 16:13:55 +0000

    Danny - Add Die prefab and die test scene

---
+++ Assets/Danny/Scripts/DisplayDieValue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayDieValue : MonoBehaviour
+{
+    [SerializeField]
+    Die die;
+    [SerializeField]
+    Button rollButton;
+    [SerializeField]
+    Button resetButton;
+
+    int dieValue;
+    // Update is called once per frame
+    void Update()
+    {
+        rollButton.interactable = die.IsReadyToRoll();
+        resetButton.interactable = die.CanReset();
+        dieValue = die.GetValue();
+        if(dieValue != 0)
+        {
+            GetComponent<Text>().text = "You rolled a " + dieValue + "!"; 
+        }
+        else
+        {
+            GetComponent<Text>().text = "";
+        }
+        
+        }
+}
