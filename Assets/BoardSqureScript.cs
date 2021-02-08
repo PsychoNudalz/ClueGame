d27e5b7e13d3e6d6769cfc1af0ed0e372451eea3
A Wong <anson.aw0618@gmail.com>
2021-02-08 22:58:42 +0000

    Anson: Started work on player picking square

---
+++ Assets/BoardSqureScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSqureScript : MonoBehaviour
+{
+    [SerializeField] Vector2 gridPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridPosition = new Vector2(transform.position.x, transform.position.z);
+    }
+
+    
+    Vector2 GetSquarePos()
+    {
+        return gridPosition;
+    }
+}
---
+++ Assets/PlayerSelectionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectionScript : MonoBehaviour
+{
+    Camera mainCamera;
+    [SerializeField] LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = FindObjectOfType<Camera>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void 
+}
