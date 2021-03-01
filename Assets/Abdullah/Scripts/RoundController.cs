5ff39e139373e5f4e6cc12029746c65551b19aac
Toast <hungrytoast99@gmail.com>
2021-03-01 14:18:50 +0200

    Added Stuff to turn controller and round controller

---
+++ Assets/Abdullah/Scripts/RoundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundController : MonoBehaviour
+{
+    TurnController player;
+    Dice dice;
+    PlayerControlScript move;
+    void RollDice() 
+    {
+        dice.RollDice();
+    }
+
+    void MovePlayer() {
+       
+    }
+
+    void MakeSuggestion() { }
+
+    void MakeAccusation() { }
+
+    void EndTurn() { }
+}
---
+++ Assets/Abdullah/Scripts/TurnController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour
+{
+    List<PlayerHanderScript> currentPlayers;
+    List<PlayerHanderScript> initialisePlayers;
+    public int currentPlayerIndex;
+
+    void Awake()
+    {
+        currentPlayers = initialisePlayers;
+        currentPlayerIndex = 0;
+    }
+
+    void GetNextPlayer() 
+    { 
+    
+    }
+
+    void SetNextPlayer() 
+    {
+    
+    }
+
+    void GetCurrentPlayer() 
+    {
+    
+    }
+
+    void SetCurrentPlayer() 
+    {
+    
+    }
+
+
+    void RemovePlayer() 
+    {
+    
+    }
+
+    void GetSuggestion() 
+    {
+    
+    }
+
+    void ShowCard() 
+    {
+    
+    }
+
+    void CallNextTurn() 
+    {
+    
+    }
+
+    void Win() 
+    {
+    
+    }
+}
