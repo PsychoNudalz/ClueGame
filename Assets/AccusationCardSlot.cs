2e19522a53f8e8c70309c48a0ff07407db1fc9d5
Daniel Newsom <danielgnewsom@gmail.com>
2021-04-22 14:54:10 +0100

    Danny -

---
+++ Assets/AccusationCardSlot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccusationCardSlot : MonoBehaviour
+{
+    public Card card;
+    public Image img;
+    public TextMeshProUGUI txt;
+    public bool isVisible = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Initialise()
+    {
+        img.sprite = null;
+        txt.text = "";
+    }
+
+    public void SetCard(Card c)
+    {
+        card = c;
+        SetCardDetails();
+    }
+
+    private void SetCardDetails()
+    {
+        if (!card)
+        {
+            return;
+        }
+        img.sprite = card.GetCardImage();
+        txt.text = EnumToString.GetStringFromEnum(card.GetCardType());
+    }
+
+    public void SetVisible(bool isVisible = true)
+    {
+        this.isVisible = isVisible;
+        gameObject.SetActive(isVisible);
+    }
+
+
+}
